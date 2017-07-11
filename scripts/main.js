4c4474a47b4b4ae857e3a8bb09a25873e93bab98
AltheaE <althea.espejo@gmail.com>
2017-07-11 13:59:42 +0200

    Change API endpoint and more quotes button function

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,17 @@
 
 const requestAuthor = new XMLHttpRequest();
 const requestQuotes = new XMLHttpRequest();
+const requestMoreQuotes = new XMLHttpRequest();
 const mainApi = 'http://localhost:8000/api/';
 const quotesApi= mainApi + 'quotes/';
+const moreQuotesApi = quotesApi + '?page=';
 const authorsApi= mainApi + 'authors/';
 const successRequest = 200;
 const failRequest = 400;
 let quotes = [];
+let getButtonQuote = document.querySelector('.button-quote');
+let counterQuote = 0;
+const quoteContainer = document.querySelector('.quotes-container');
 
 
 
@@ -18,14 +23,13 @@ function getApiQuotes() {
   requestQuotes.onload = function() {
     if (requestQuotes.status >= successRequest && requestQuotes.status < failRequest) {
       let data = JSON.parse(requestQuotes.responseText);
-      for (var i = 0; i < data.length; i++) {
+      for (var i = 0; i < data.results.length; i++) {
        let quote = {
-         text: data[i].text,
+         text: data.results[i].text,
       };
       quotes.push(quote);
       printQuotes();
       }
-      console.log(quotes);
     } else {
       console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
     }
@@ -43,7 +47,6 @@ function getApiQuotes() {
 getApiQuotes();
 
 function printQuotes() {
-  const quoteContainer = document.querySelector('.quotes-container');
   quoteContainer.innerHTML = "";
   for (var i = 0; i < quotes.length; i++) {
    quoteContainer.innerHTML += `
@@ -51,3 +54,33 @@ function printQuotes() {
    `;
 }
 }
+
+function getMoreQuotes() {
+  requestMoreQuotes.open('GET', moreQuotesApi + counterQuote, true);
+
+  requestMoreQuotes.onload = function() {
+    counterQuote = counterQuote + 1;
+    if (requestMoreQuotes.status >= successRequest && requestMoreQuotes.status < failRequest) {
+      let data = JSON.parse(requestMoreQuotes.responseText);
+      for (var i = 0; i < data.results.length; i++) {
+       let quote = {
+         text: data.results[i].text,
+      };
+      quotes.push(quote);
+      printmoreQuotes();
+      }
+    } else {
+      console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
+    }
+    return quotes;
+  };
+
+  requestMoreQuotes.onerror = function() {
+    console.log('Error al tratar de conectarse con el servidor');
+  };
+
+  requestQuotes.send();
+  return quotes;
+  }
+
+getButtonQuote.addEventListener( 'click' , getMoreQuotes);
