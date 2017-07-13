bd2712736f4594c17ef7f804665d3d4da2714b35
Esther Lozano <elozano@autentia.com>
2017-07-13 14:06:50 +0200

    Refactor ajax

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,7 @@
 'use strict';
 //////////// API request ///////////
-const requestAuthor = new XMLHttpRequest();
-const requestQuotes = new XMLHttpRequest();
-const requestAuthorsList = new XMLHttpRequest();
-const mainApi = 'http://verba.piweek.com/api/';
+const mainApi = 'http://verba.piweek.com:8000/api/';
 const authorsApi = mainApi + 'authors/?page_size=200';
-const successRequest = 200;
-const failRequest = 400;
 let quotes = [];
 let authors = [];
 let getButtonQuote = document.querySelector('.js-load-quotes-btn');
@@ -20,36 +15,24 @@ const closeFilterButton = document.querySelector('.js-close-btn');
 
 function getApiQuotes() {
 
-  requestQuotes.open('GET', nextQuoteUrl, true);
+    $.get(nextQuoteUrl, function(quotesData, status) {
+        if (status === 'success') {
+            nextQuoteUrl = quotesData.next;
 
-  requestQuotes.onload = function() {
-    if (requestQuotes.status >= successRequest && requestQuotes.status < failRequest) {
-      let quotesData = JSON.parse(requestQuotes.responseText);
-      nextQuoteUrl = quotesData.next;
-
-      for (var i = 0; i < quotesData.results.length; i++) {
-        let quote = {
-          text: quotesData.results[i].text,
-          author: quotesData.results[i].author.name,
-        };
-        quotes.push(quote);
-        printQuotes();
-      }
-    } else {
-      console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
-    }
-    return quotes;
-  };
-
-  requestQuotes.onerror = function() {
-    console.log('Error al tratar de conectarse con el servidor');
-  };
-
-  requestQuotes.send();
-  return quotes;
+            for (var i = 0; i < quotesData.results.length; i++) {
+                let quote = {
+                    text: quotesData.results[i].text,
+                    author: quotesData.results[i].author.name,
+                };
+                quotes.push(quote);
+                printQuotes();
+            }
+        } else {
+            console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
+        }
+        return quotes;
+    });
 }
-
-getApiQuotes();
 
 function printQuotes() {
   quoteContainer.innerHTML = '';
@@ -74,50 +57,41 @@ function printQuotes() {
 }
 
 
-getButtonQuote.addEventListener('click', getApiQuotes);
-
 function getAuthorsList() {
 
-  requestAuthorsList.open('GET', authorsApi, true);
+    $.get(authorsApi, function(authorsData, status) {
+        if (status === 'success') {
+            for (var i = 0; i < authorsData.results.length; i++) {
+                let author = {
+                    name: authorsData.results[i].name,
+                    url: authorsData.results[i].url,
+                };
+                authors.push(author);
+            }
+            console.log(authors);
+        } else {
+            console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
+        }
+        return authors;
+    });
 
-  requestAuthorsList.onload = function() {
-    if (requestAuthorsList.status >= successRequest && requestAuthorsList.status < failRequest) {
-      let authorsData = JSON.parse(requestAuthorsList.responseText);
-      for (var i = 0; i < authorsData.results.length; i++) {
-        let author = {
-          name: authorsData.results[i].name,
-          url: authorsData.results[i].url,
-        };
-        authors.push(author);
-      }
-      console.log(authors);
-    } else {
-      console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
-    }
-    return authors;
-  };
-
-  requestAuthorsList.onerror = function() {
-    console.log('Error al tratar de conectarse con el servidor');
-  };
-
-  requestAuthorsList.send();
-  return authors;
 }
 
-getAuthorsList();
-
 function printAuthors() {
-  authorsContainer.innerHTML = '';
-  for (var i = 0; i < authors.length; i++) {
-    authorsContainer.innerHTML += `
+    authorsContainer.innerHTML = '';
+    for (var i = 0; i < authors.length; i++) {
+        authorsContainer.innerHTML += `
     <div class="author-btn">
       ${authors[i].name}
     </div>
    `;
-  }
+    }
 }
 
+getApiQuotes();
+getAuthorsList();
+
+getButtonQuote.addEventListener('click', getApiQuotes);
 authorsButton.addEventListener('click', printAuthors);
 
 //////////// Modal///////////
